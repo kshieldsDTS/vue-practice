32a7578497b4eaebb7977169639643c560feac8a
kshieldsDTS <escanor05@gmail.com>
2022-03-31 15:59:25 -0400

    refactor button methods to allow for reusability

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <header>
         <h1>{{ title }}</h1>
-        <AppButton @toggle-add-task="$emit('toggle-add-task')" :text="showAddTask ? 'Close' : 'Add Task'" :color="showAddTask ? 'red' : 'green'"/>
+        <AppButton @btn-click="$emit('toggle-add-task')" :text="showAddTask ? 'Close' : 'Add Task'" :color="showAddTask ? 'red' : 'green'"/>
     </header>
 </template>
 
